8ece19b8a394958e7252349eb5443df74650c4d2
Evgeny Antipov <e.antipov@gmail.com>
2022-07-22 21:54:00 +0300

    Added the calculation of the average

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,24 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = max();
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int average() {
+        int sum = 0;
+        if (sales.length >= 3) {
+            for (int sale : sales) {
+                sum += sale;
+            }
+        }
+        return (sum - max() - min()) / (sales.length - 2);
+    }
 }
